f70ae62a1bb7ab60393b4122a98077af2fcfcd3e
LIXU1 <18801287297@163.com>
2019-03-26 20:47:03 +0800

    首页

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 
 
 App({
+  // 全局数据，类似于store
+  globalData: {
+    hasLogin: false,
+    openid: null
+  },
   onLaunch: function (opts) {
     console.log('App Launch', opts)
   },
   onShow: function (opts) {
     console.log('App Show', opts)
-  },
-  onHide: function () {
-    console.log('App Hide')
-  },
-  globalData: {
-    hasLogin: false,
-    openid: null
   },
 })
