b90e6d37b463f6665537a3ed712d248188765691
LIXU1 <18801287297@163.com>
2019-04-02 14:20:20 +0800

    我的板块页面

--- page/tabBar/mine/mine.js
+++ page/tabBar/mine/mine.js
@@ -1,66 +1,34 @@
-// page/tabBar/mine/mine.js
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    blockIsShow:false
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
   onHide: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  //实名认证
+  goAuthentication:function(){
+    wx.navigateTo({
+      url: '/page/tabBar/minePages/authentication/authentication',
+    })
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  // 我的项目
+  goMyProject: function () {
+    wx.navigateTo({
+      url: '/page/tabBar/minePages/myProject/myProject',
+    })
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+  // 个人资料
+  goPersonData: function () {
+    wx.navigateTo({
+      url: '/page/tabBar/minePages/personData/personData',
+    })
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+  
 })
---
+++ page/tabBar/minePages/myProject/myProject.js
@@ -0,0 +1,40 @@
+
+Page({
+  data: {
+    selectStatus: 0,
+    headerBorder: true,//header添加border
+  },
+  onLoad: function (options) {
+
+  },
+  onShow: function () {
+
+  },
+  onHide: function () {
+
+  },
+  // header切换
+  myProject: function () {
+    this.setData({
+      headerBorder: true
+    })
+  },
+  youProject: function () {
+    this.setData({
+      headerBorder: false
+    })
+  },
+  // 项目
+  peojectTap: function () {
+    this.setData({
+      selectStatus: 1
+    })
+  },
+  // 班组
+  classTap: function () {
+    this.setData({
+      selectStatus: 2
+    })
+  },
+
+})
